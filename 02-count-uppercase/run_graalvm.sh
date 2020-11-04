5e4259bb904b40d4e780dd4878725bc1f2eedced
Boris Spasojevic <boris.spasojevic@oracle.com>
2020-11-04 16:22:30 +0100

    Separate run files.

---
+++ 02-count-uppercase/run_graalvm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -ex
+
+$GRAALVM_HOME/bin/java CountUppercase In 2021 I would like to run ALL languages in one VM.
---
+++ 02-count-uppercase/run_java.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -ex
+
+java CountUppercase In 2021 I would like to run ALL languages in one VM.
+
